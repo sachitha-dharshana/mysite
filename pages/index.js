f321e0a9ee15656cfa9dc116e7f8b5d76c63d44a
Sachitha Dharshana <sachitha.dharshana@gmail.com>
2021-07-08 21:54:39 +0530

    Added meta description

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,10 @@ export default function Home() {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Personal portfolio website of Sachitha Dharshana"
+        />
         <link rel="icon" href="/favicon.png" />
         <link
           rel="stylesheet"
